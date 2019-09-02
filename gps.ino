0718189c953950774016abb7cd9a51d5943995d8
vladbondarenko <vbondarenko@boylesoftware.com>
2019-09-02 17:18:47 +0300

    Update gps.ino

--- gps.ino
+++ gps.ino
@@ -5,50 +5,63 @@
 #define DEBUG 1
 
 SoftwareSerial GPS(RxD, TxD);
-String result;
-String data[5];
-String state,timegps,latitude,longitude;
+
+String result,payload,flag;
+String data[7];
+//String state,timegps,latitude,longitude,alt,sp;
 char c;
+int r;
 void setup()
 {
   Serial.begin(9600);
   GPS.begin(9600);
+  delay(1000);
   pinMode(4, OUTPUT);
+  //sendData("\r\n+ATI\r\n",50,DEBUG);
+  //delay(1000);
+  //if (flag == 0) {
   digitalWrite(4, HIGH);
   delay(2000);
   digitalWrite(4, LOW);
   delay(10000);
-  sendData("\r\n+ATI\r\n",50,DEBUG);
-  //GPS.print("\r\n+ATE1\r\n");
-  //result = GPS.readString();Serial.println(result);
+  //}
+  //sendData("\r\n+ATI\r\n",50,DEBUG);
+  //Serial.println(flag);
   sendData("\r\n+AT+CGNSPWR=1\r\n",50,DEBUG);
   sendData("\r\n+AT+ECHARGE=1\r\n",50,DEBUG);
   sendData("\r\n+AT+CGNSSEQ=RMC\r\n",100,DEBUG);
   sendData("\r\n+AT+SAPBR=3,1,CONTYPE,GPRS\r\n",1000,DEBUG);
   sendData("\r\n+AT+SAPBR=3,1,APN,internet\r\n",1000,DEBUG);
-  sendData("\r\n+AT+SAPBR=1,1\r\n",1000,DEBUG);
-  sendData("\r\n+AT+HTTPINIT\r\n",500,DEBUG);
-  sendData("\r\n+AT+HTTPPARA=CID,1\r\n",500,DEBUG);
+ // sendData("\r\n+AT+HTTPINIT\r\n",500,DEBUG);
+  r = 0;
 }
 
 void loop()
 {
-
-  state = "0";
-  timegps = "0";
-  latitude = "0";
-  longitude = "0";
-
- sendTabData("AT+CGNSINF",1000,DEBUG);
-if (state !=0) {
-//    Serial.println("State  :"+state);
-//    Serial.println("Time  :"+timegps);
-//    Serial.println("Latitude  :"+latitude);
-//    Serial.println("Longitude  :"+longitude);
-}
-  sendData((String)"\r\n+AT+HTTPPARA=URL,http://gps.XXXXXXX.com/g/00001/"+timegps+":"+latitude+":"+longitude+"\r\n",1000,DEBUG);
-  sendData("\r\n+AT+HTTPACTION=0\r\n",2000,DEBUG);
-  GPS.println((char)26);
+  r++;
+  Serial.println((String)"Step:"+r);
+  if (r > 30){
+    sendData("\r\n+AT+CFUN=1,1\r\n",50,DEBUG);
+    delay(10000);
+    sendData("\r\n+AT+CGNSPWR=1\r\n",50,DEBUG);
+    sendData("\r\n+AT+ECHARGE=1\r\n",50,DEBUG);
+    sendData("\r\n+AT+CGNSSEQ=RMC\r\n",100,DEBUG);
+    sendData("\r\n+AT+SAPBR=3,1,CONTYPE,GPRS\r\n",1000,DEBUG);
+    sendData("\r\n+AT+SAPBR=3,1,APN,internet\r\n",1000,DEBUG);
+    r = 0;
+  }
+  sendData("\r\n+AT+HTTPINIT\r\n",500,DEBUG);
+  sendData("\r\n+AT+SAPBR=1,1\r\n",1000,DEBUG);
+  sendData("\r\n+AT+HTTPPARA=CID,1\r\n",500,DEBUG);
+  sendTabData("\r\n+AT+CGNSINF\r\n",2000,DEBUG);
+  Serial.flush();
+  payload = "";
+  payload = (String)"\r\n+AT+HTTPPARA=URL,http://gps.XXXXXXX.com/g/1/"+data[2]+":"+data[3]+":"+data[4]+":"+data[5]+":"+data[6]+"\r\n";
+  sendData (payload,500, DEBUG);
+  delay(100);
+  sendData("\r\n+AT+HTTPPARA=\"BREAK\",3000",3000,DEBUG);
+  sendData("\r\n+AT+HTTPACTION=0\r\n",3000,DEBUG);
+  sendData("\r\n+AT+HTTPTERM\r\n",500,DEBUG);
 }
 void sendTabData(String command , const int timeout , boolean debug){
   GPS.println(command);
@@ -60,32 +73,38 @@ void sendTabData(String command , const int timeout , boolean debug){
   data[3] = "";
   data[4] = "";
   data[5] = "";
+  data[6] = "";
+  data[7] = "";
   while((time+timeout) > millis()){
     while(GPS.available()){
       c = GPS.read();
+      //Serial.print(c);
       if (c != ',') {
          data[i] +=c;
-         delay(100);
+         delay(40);
       } else {
         i++;  
       }
-      if (i == 5) {
-        delay(100);
+      if (i == 7) {
+        delay(40);
         goto exitL;
       }
     }
   }exitL:
   if (debug) {
-    state = data[1];
-    timegps = data[2];
-    latitude = data[3];
-    longitude =data[4];  
+//    state = data[1];
+//    timegps = data[2];
+//    latitude = data[3];
+//    longitude = data[4];
+//    alt = data[5];
+//    sp = data[6];
   }
 }
 
 void sendData(String command, const int timeout, boolean debug)
 {
-    String response = "";    
+  flag = "0";
+    String response = "";
     GPS.println(command); 
     long int time = millis();   
     while( (time+timeout) > millis()){
@@ -95,6 +114,10 @@ void sendData(String command, const int timeout, boolean debug)
     }    
     if(debug){
       Serial.print(response);
-    }    
+    }
+    //Checking if GPS board is on.
+    if (command == "\r\n+ATI\r\n" && (sizeof(response) > 5)){flag = 1;}
+   // Serial.println(response);
+    GPS.flush();
     return(response);
 }
